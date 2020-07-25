5fc3c7e8012ea37c231957f45e8861c7cbe1214a
MahfuzRonnie <rony@mail.ynu.edu.cn>
2020-07-25 15:53:02 +0600

    Ages

---
+++ 1154.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,n=0;
+    float avg,j=0;
+    while(1)
+    {
+        scanf("%d",&a);
+        if(a<0)
+            break;
+        n+=a;
+        j++;
+    }
+    avg=n/j;
+    printf("%.2f\n",avg);
+    return 0;
+}
