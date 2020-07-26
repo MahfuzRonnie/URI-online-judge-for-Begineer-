727a8a579d7614705d2febacb509b18c6e9ad4d4
MahfuzRonnie <rony@mail.ynu.edu.cn>
2020-07-26 11:59:39 +0600

    Array fill I

---
+++ 1173.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int N[10],i;
+    scanf("%d",&N[0]);
+    for(i=1;i<=9;i++)
+    {
+        N[i]=N[i-1]*2;
+    }
+    for(i=0;i<=9;i++)
+    {
+        printf("N[%d] = %d\n",i,N[i]);
+    }
+    return 0;
+}
